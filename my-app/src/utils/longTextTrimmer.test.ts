cc06eff0472a483559adc130ac1181996086bab2
christopher p. k. morris <slime@christohersmini.hq.k.grp>
2018-10-31 14:21:17 +0000

    regEx tests passing and working

--- my-app/src/utils/longTextTrimmer.test.ts
+++ my-app/src/utils/longTextTrimmer.test.ts
@@ -1,10 +1,31 @@
-// import trimTitleOfTodoDown from "./longTextTrimmer";
+import trimTitleOfTodoDown from "./longTextTrimmer";
 
+describe('test regEx works properly', () => {
 
-const testString: string = "cat "; 
+  test('test regEx: \\w\\s$ appends ... correctly', () => {
+      const testString: string = "cat "; 
+      expect(trimTitleOfTodoDown(testString, testString.length)).toBe("cat...");
+    });
 
-test('test regEx: \\w\\s appends .. correctly', () => {
-    expect(2 + 2).toBe(4);
+  test('test regEx: \\w\\s\\w$ appends ... correctly', () => {
+    const testString: string = "cat t"; 
+    expect(trimTitleOfTodoDown(testString, testString.length)).toBe("cat...");
   });
 
-  
+  test('test regEx: \\w\\$ appends ... correctly', () => {
+    const testString: string = "sdsdfsdfsdafcat"; 
+    expect(trimTitleOfTodoDown(testString, testString.length)).toBe("sdsdfsdfsdafcat...");
+  }); 
+
+  test('test regEx: empty string returns no elipses', () => {
+    const testString: string = ""; 
+    expect(trimTitleOfTodoDown(testString, testString.length)).toBe("");
+  }); 
+
+  test('test regEx: \\s+ returns just ...', () => {
+    const testString: string = "            "; 
+    expect(trimTitleOfTodoDown(testString, testString.length)).toBe("...");
+  }); 
+
+}); 
+
--- my-app/src/utils/longTextTrimmer.ts
+++ my-app/src/utils/longTextTrimmer.ts
@@ -3,7 +3,15 @@
 export default function trimTitleOfTodoDown(title: string, cutOffPoint: number): string{
     let stringWithCutOffPoint: string = title.substring(0, cutOffPoint); 
     if(stringWithCutOffPoint.length == cutOffPoint){
-        return stringWithCutOffPoint.replace(/(\w+)(?:\s+\w$|\s+$|$)/g, "$1...") as string; 
+        let regExSearchAndReplaceEndOfString: RegExp = /(\w+)(?:\s+\w$|\s+$|$)/g; 
+        if(regExSearchAndReplaceEndOfString.exec(stringWithCutOffPoint) != null){
+            console.log("normal case"); 
+            return stringWithCutOffPoint.replace(regExSearchAndReplaceEndOfString, "$1...") as string; 
+        }else{
+            return stringWithCutOffPoint.replace(/\s+/g, "...") as string; 
+        }
+    }else if(/^\s+$/g.exec(stringWithCutOffPoint) != null){
+        return stringWithCutOffPoint.replace(/\s+/g, "...") as string; 
     }else{
         return stringWithCutOffPoint; 
     }
